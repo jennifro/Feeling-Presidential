106baa5e1ff71d8990437b488b2b65e288eba33d
Jennifer <jendixon85@gmail.com>
2016-08-14 02:23:57 +0000

    Working basic flask server, no templates.

---
+++ server.py
@@ -0,0 +1,18 @@
+from flask import Flask
+# from flask_debugtoolbar import DebugToolbarExtension
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    html = "<html><body><h1>Homepage here!</h1></body></html>"
+    return html
+
+
+if __name__ == "__main__":
+    app.debug = True
+
+    # DebugToolbarExtension(app)
+
+    app.run(host="0.0.0.0")
