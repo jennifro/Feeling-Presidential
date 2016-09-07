174971be36b31cd54bb2a5b3db4abab764752cf1
Jennifer <jendixon85@gmail.com>
2016-09-07 01:08:43 +0000

    Added sample data to db for testing.

--- model.py
+++ model.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 db = SQLAlchemy()
 
-DB_URI = 'postgresql:///speeches'
+# DB_URI = 'postgresql:///speeches'
 
 ## TODO: FIGURE OUT AUTOINCREMENT RESET
 
@@ -40,8 +40,6 @@ class Speech(db.Model):
     sentiment = db.Column(db.String(25))
 
     prez = db.relationship('President')
-
-    # speechtype = db.relationship('SpeechTypes')
 
     speech_phrases = db.relationship('SpeechCollocation')
 
@@ -81,12 +79,45 @@ class SpeechCollocation(db.Model):
         return '<ID={}, Speech ID={}, Phrase ID={}>'.format(self.connect_id, self.speech_id, self.phrase_id)
 
 
-def connect_to_db(app):
+def connect_to_db(app, db_uri='postgresql:///speeches'):
     """Connect database to Flask app."""
 
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///speeches'
+    app.config['SQLALCHEMY_DATABASE_URI'] = db_uri
     db.app = app
     db.init_app(app)
+
+
+def test_data():
+    """Create sample data for testing."""
+
+    President.query.delete()
+    Speech.query.delete()
+    Collocation.query.delete()
+    SpeechCollocation.query.delete()
+
+    p1 = President(name='Kanye West')
+    p2 = President(name='Beyonce Knowles')
+    p3 = President(name='Janelle Monae')
+
+    s1 = Speech(title='State of the Union (January 7, 2020)', sentiment='pos',
+                speaker=p1.prez_id)
+    s2 = Speech(title='State of the Union (January 25, 2024)', sentiment='neg',
+                speaker=p2.prez_id)
+    s3 = Speech(title='State of the Union (January 10, 2028)', sentiment='pos',
+                speaker=p3.prez_id)
+
+    c1 = Collocation(phrase='climb spaceship', sentiment_score='negative')
+    c2 = Collocation(phrase='formation coordination', sentiment_score='neutral')
+    c3 = Collocation(phrase='electric lady', sentiment_score='positive')
+
+    sc1 = SpeechCollocation(phrase_id=c1.phrase_id, speech_id=s1.speech_id)
+    sc2 = SpeechCollocation(phrase_id=c2.phrase_id, speech_id=s2.speech_id)
+    sc3 = SpeechCollocation(phrase_id=c3.phrase_id, speech_id=s3.speech_id)
+
+    db.session.add_all([p1, p2, p3, s1, s2, s3, c1, c2, c3, sc1, sc2, sc3])
+    db.session.commit()
+
+#################
 
 if __name__ == '__main__':
     from server import app
