a1cd91a927ec273f902c1f693344fc4d5fb219ea
Jennifer <jendixon85@gmail.com>
2016-09-11 21:14:45 +0000

    Removed speechescrape file; scraper was not done in that file.

--- speechescrape/speechescrape/items.py
+++
@@ -1,9 +0,0 @@
-import scrapy
-
-
-class SpeechescrapeItem(scrapy.Item):
-    """Relevant data from scrapy crawl."""
-
-    title = scrapy.Field()
-    author = scrapy.Field()
-    text = scrapy.Field()
